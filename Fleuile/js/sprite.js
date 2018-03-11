59054024e380e4c429e9da411c0bed70a6630a49
manamiD1013 <manami.d.zeki1013@gmail.com>
2018-03-11 18:23:22 +0900

    あ

--- Fleuile/js/sprite.js
+++ Fleuile/js/sprite.js
@@ -1,19 +1,14 @@
 $(function(){
-   // #で始まるアンカーをクリックした場合に処理
-   $('a[href^=#]').click(function() {
-      // スクロールの速度
-      var speed = 400; // ミリ秒
-      // アンカーの値取得
-      var href= $(this).attr("href");
-      // 移動先を取得
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      // 移動先を数値で取得
-      var position = target.offset().top;
-      // スムーススクロール
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-   });
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
 });
+
 $(function(){
 var topBtn=$('#pageTop');
 topBtn.hide();
